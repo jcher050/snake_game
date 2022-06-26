961a0b1cd7df630063e869758eb0c463cd656662
jcher050 <35442284+jcher050@users.noreply.github.com>
2022-06-26 17:37:18 -0400

    .

--- snakeapp.py
+++ snakeapp.py
@@ -1,4 +1,5 @@
 from dis import dis
+import py_compile
 import pygame
 import time
 import random
@@ -53,4 +54,40 @@ def gameloop():
     y1_change = 0
 
     snake_list = []
-    Lemgth_of_snake = 1
+    Length_of_snake = 1
+
+    foodx = round(random.randrange(0, dis_width-snake_block)/10.0)*10.0
+    foody = round(random.randrange(0, dis_height-snake_block)/10.0)*10.0
+
+    while not game_over:
+
+        while game_close == True:
+            dis.fill(blue)
+            message("You Lost! Press 'C' to Play Again or 'Q' To Quit The Game", red)
+            Your_score(Length_of_snake-1)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameloop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.key == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    x1_change = snake_block
+                    y1_change = 0
