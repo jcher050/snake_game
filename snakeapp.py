ab53c5b1e2273022ac4f7f82e180ad0bd119dc71
jcher050 <35442284+jcher050@users.noreply.github.com>
2022-06-27 23:17:54 -0400

    .
    .

--- snakeapp.py
+++ snakeapp.py
@@ -1,5 +1,7 @@
 from dis import dis
+from operator import truediv
 import py_compile
+from xml.etree.ElementPath import get_parent_map
 import pygame
 import time
 import random
@@ -91,3 +93,40 @@ def gameloop():
                 elif event.key == pygame.K_DOWN:
                     x1_change = snake_block
                     y1_change = 0
+
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+            game_close = True
+
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(Blue)
+        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_list.append(snake_Head)
+        if len(snake_list) > Length_of_snake:
+            del snake_List[0]
+
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+
+        our_snake(snake_block, snake_List)
+        Your_score(Length_of_snake - 1)
+
+        pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, dis_width-snake_block)/10.0)*10.0
+            foody = round(random.randrange(
+                0, dis_height-snake_block)/10.0)*10.0
+            Length_ofsnake += 1
+
+        clock.tick(snake_speed)
+
+    pygame.quit()
+    quit()
+
+
+gameLoop()
